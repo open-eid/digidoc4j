3200394acbf746cb6988d98bcb31160015b70f0b
Aho Augasmägi <aho@codeborne.com>
2014-06-09 12:53:43 +0000

    changed package structure

---
+++ test/org/digidoc4j/ContainerTest.java
@@ -0,0 +1,65 @@
+package org.digidoc4j;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+
+import org.digidoc4j.exceptions.NotYetImplementedException;
+import org.digidoc4j.utils.PKCS12Signer;
+import org.junit.Test;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+public class ContainerTest {
+  @Test
+  public void testAddOneFileToContainer() throws Exception {
+    Container bDocContainer = new Container();
+    bDocContainer.addDataFile("test.txt", "text/plain");
+    List<DataFile> dataFiles = bDocContainer.getDataFiles();
+    assertEquals(1, dataFiles.size());
+    assertEquals("test.txt", dataFiles.get(0).getFileName());
+    assertEquals("text/plain", dataFiles.get(0).getMediaType());
+  }
+
+  @Test(expected = NotYetImplementedException.class)
+  public void testAddDataFileFromInputStreamToContainer() throws Exception {
+    Container container = new Container();
+    container.addDataFile(new ByteArrayInputStream(new byte[]{0x41}), "test.txt", "text/plain");
+  }
+
+  @Test
+  public void testRemovesOneFileFromContainerWhenFileExists() throws Exception {
+    Container bDocContainer = new Container();
+    bDocContainer.addDataFile("test.txt", "text/plain");
+    bDocContainer.removeDataFile("test.txt");
+
+    assertEquals(0, bDocContainer.getDataFiles().size());
+  }
+
+  public void testSigningWithSignerInfo() throws Exception {
+    String city = "myCity";
+    String stateOrProvince = "myStateOrProvince";
+    String postalCode = "myPostalCode";
+    String country = "myCountry";
+    String signerRoles = "myRole / myResolution";
+
+    PKCS12Signer signer = new PKCS12Signer("signout.p12", "test");
+    signer.setSignatureProductionPlace(city, stateOrProvince, postalCode, country);
+    signer.setSignerRoles(asList(signerRoles));
+
+    Container bDocContainer = new Container();
+    bDocContainer.addDataFile("test.txt", "text/plain");
+    Signature signature = bDocContainer.sign(signer);
+  }
+
+  public void testSigningWithOnlyLocationInfo() throws Exception {
+  }
+
+  public void testSigningWithPartialSignerInfo() throws Exception {
+  }
+
+  public void testSigningWithOnlySignerRole() throws Exception {
+  }
+}
+
+
---
+++ test/org/digidoc4j/X509CertTest.java
@@ -0,0 +1,103 @@
+package org.digidoc4j;
+
+import java.io.IOException;
+import java.security.cert.X509Certificate;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static java.util.Arrays.asList;
+import static org.digidoc4j.X509Cert.SubjectName.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class X509CertTest {
+
+  private static X509Cert cert;
+  private final int ONE_DAY = 1000 * 60 * 60 * 24;
+  private final SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+
+  @BeforeClass
+  public static void setUp() throws Exception {
+    cert = new X509Cert("signout.pem");
+  }
+
+  @Test
+  public void testGetX509Certificate() throws Exception {
+    X509Certificate x509Certificate = cert.getX509Certificate();
+    assertEquals("SERIALNUMBER=11404176865, GIVENNAME=MÄRÜ-LÖÖZ, SURNAME=ŽÕRINÜWŠKY, " +
+                 "CN=\"ŽÕRINÜWŠKY,MÄRÜ-LÖÖZ,11404176865\", OU=digital signature, O=ESTEID, C=EE",
+                 x509Certificate.getSubjectDN().getName());
+  }
+
+  @Test
+  public void testGetSerialNumber() {
+    assertEquals("497c5a2bfa9361a8534fbed9f48e7a12", cert.getSerial());
+  }
+
+  @Test
+  public void testGetIssuerName() {
+    assertEquals("emailaddress=pki@sk.ee, cn=test of esteid-sk 2011, o=as sertifitseerimiskeskus, c=ee",
+                 cert.issuerName().toLowerCase());
+  }
+
+  @Test
+  public void testGetIssuerNameByPart() {
+    assertEquals("pki@sk.ee", cert.issuerName(X509Cert.Issuer.EMAILADDRESS).toLowerCase());
+    assertEquals("as sertifitseerimiskeskus", cert.issuerName(X509Cert.Issuer.O).toLowerCase());
+    assertEquals("test of esteid-sk 2011", cert.issuerName(X509Cert.Issuer.CN).toLowerCase());
+    assertEquals("ee", cert.issuerName(X509Cert.Issuer.C).toLowerCase());
+  }
+
+  @Test
+  public void testGetPolicies() throws IOException {
+    assertEquals(1, cert.getCertificatePolicies().size());
+  }
+
+  @Test
+  public void testIsValidAtSpecifiedDate() {
+    assertTrue(cert.isValid(new Date()));
+  }
+
+  @Test
+  public void testIsNotValidYet() throws ParseException {
+    Date certValidFrom = dateFormat.parse("17.04.2014");
+    assertFalse(cert.isValid(new Date(certValidFrom.getTime() - ONE_DAY)));
+  }
+
+  @Test
+  public void testIsNoLongerValid() throws ParseException {
+    Date certValidFrom = dateFormat.parse("12.04.2016");
+    assertFalse(cert.isValid(new Date(certValidFrom.getTime() + ONE_DAY)));
+  }
+
+  @Test
+  public void testIsCertValidToday() {
+    assertTrue(cert.isValid());
+  }
+
+  @Test
+  public void testKeyUsage() {
+    assertEquals(asList(X509Cert.KeyUsage.NON_REPUDIATION), cert.getKeyUsages());
+  }
+
+  @Test
+  public void testGetPartOfSubjectName() throws Exception {
+    assertEquals("11404176865", cert.getSubjectName(SERIALNUMBER));
+    assertEquals("märü-lööz", cert.getSubjectName(GIVENNAME).toLowerCase());
+    assertEquals("žõrinüwšky", cert.getSubjectName(SURNAME).toLowerCase());
+    assertEquals("\"žõrinüwšky,märü-lööz,11404176865\"", cert.getSubjectName(CN).toLowerCase());
+    assertEquals("digital signature", cert.getSubjectName(OU).toLowerCase());
+    assertEquals("esteid", cert.getSubjectName(O).toLowerCase());
+    assertEquals("ee", cert.getSubjectName(C).toLowerCase());
+  }
+
+  @Test
+  public void testGetSubjectName() throws Exception {
+    assertEquals("SERIALNUMBER=11404176865, GIVENNAME=MÄRÜ-LÖÖZ, SURNAME=ŽÕRINÜWŠKY, CN=\"ŽÕRINÜWŠKY,MÄRÜ-LÖÖZ,11404176865\", OU=digital signature, O=ESTEID, C=EE", cert.getSubjectName());
+  }
+}
