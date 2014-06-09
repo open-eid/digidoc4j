4b4f5af2fd15f13cbc4f152f9f25e1d1995d721d
Aho Augasmägi <aho@codeborne.com>
2014-06-09 17:39:40 +0300

    sets exception to runtime exception now

--- src/org/digidoc4j/exceptions/NotYetImplementedException.java
+++ src/org/digidoc4j/exceptions/NotYetImplementedException.java
@@ -4,5 +4,5 @@ package org.digidoc4j.exceptions;
  * Exception is thrown when method or class is not yet implemented.
  */
 
-public class NotYetImplementedException extends Exception {
+public class NotYetImplementedException extends RuntimeException {
 }
