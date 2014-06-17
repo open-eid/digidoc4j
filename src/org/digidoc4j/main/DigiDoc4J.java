a3cc3346cd3ebb7746762e71978636d646023b40
Aho Augasmägi <aho@codeborne.com>
2014-06-17 14:24:34 +0300

    adds command line client

--- src/org/digidoc4j/main/DigiDoc4J.java
+++ src/org/digidoc4j/main/DigiDoc4J.java
@@ -1,8 +1,16 @@
 package org.digidoc4j.main;
 
+import java.io.File;
+import java.util.List;
+
+import org.apache.commons.cli.*;
 import org.digidoc4j.api.Container;
+import org.digidoc4j.api.Signature;
 import org.digidoc4j.api.Signer;
+import org.digidoc4j.api.exceptions.DigiDoc4JException;
 import org.digidoc4j.utils.PKCS12Signer;
+
+import static org.digidoc4j.ContainerInterface.DocumentType.DDOC;
 
 /**
  * Client commandline tool for DigiDoc4J library.
@@ -18,11 +26,79 @@ public final class DigiDoc4J {
    * @throws Exception throws exception if the command cannot be executed successfully
    */
   public static void main(String[] args) throws Exception {
-    Container container = new Container();
-    container.addDataFile("test.txt", "text/plain");
-    Signer pkcs12Signer = new PKCS12Signer("signout.p12", "test");
+    Options options = createParameters();
+
+    CommandLine commandLine = new BasicParser().parse(options, args);
+
+    if (commandLine.getOptions().length == 0) {
+      HelpFormatter formatter = new HelpFormatter();
+      formatter.printHelp("digido4j", options);
+      System.exit(1);
+    }
+
+    run(commandLine);
+
+    System.exit(0);
+  }
+
+  private static void run(CommandLine commandLine) {
+    boolean fileHasChanged = false;
+    String inputFile = commandLine.getOptionValue("in");
+    Container container = new Container(DDOC);
+
+    if (new File(inputFile).exists())
+      container = new Container(inputFile);
+
+    if (commandLine.hasOption("add")) {
+      container.addDataFile(commandLine.getOptionValue("add"), "text/plain");
+      fileHasChanged = true;
+    }
+
+    if (commandLine.hasOption("pkcs12")) {
+      pkcs12Sign(commandLine, container);
+      fileHasChanged = true;
+    }
+
+    if (fileHasChanged)
+      container.save(inputFile);
+
+    if (commandLine.hasOption("verify"))
+      verify(container);
+  }
+
+  private static void pkcs12Sign(CommandLine commandLine, Container container) {
+    String[] optionValues = commandLine.getOptionValues("pkcs12");
+    Signer pkcs12Signer = new PKCS12Signer(optionValues[0], optionValues[1]);
     container.sign(pkcs12Signer);
-    container.save("doc.bdoc");
+  }
+
+  private static void verify(Container container) {
+    List<Signature> signatures = container.getSignatures();
+    for (Signature signature : signatures) {
+      List<DigiDoc4JException> validationResult = signature.validate();
+      if (validationResult.size() == 0) {
+        System.out.println("Signature " + signature.getId() + " is valid");
+      }
+      else {
+        System.out.println("Signature " + signature.getId() + " is not valid");
+        for (DigiDoc4JException exception : validationResult) {
+          System.out.println(exception.getMessage());
+        }
+      }
+    }
+  }
+
+  private static Options createParameters() {
+    Options options = new Options();
+    options.addOption("v", "verify", false, "verify command");
+    options.addOption(OptionBuilder.withArgName("file").hasArg().withDescription("open or creates container").create("in"));
+    options.addOption(OptionBuilder.withArgName("file").hasArg().withDescription("adds file to container").create("add"));
+    options.addOption(OptionBuilder.withArgName("pkcs12Keystore password").hasArgs(2).withValueSeparator(' ')
+                        .withDescription("sets pkcs12 keystore and keystore password").create("pkcs12"));
+    options.addOption(OptionBuilder.withArgName("slot pin").hasArgs(2).withValueSeparator(' ')
+                        .withDescription("sets pkcs11 singner slot and pin").create("pkcs11"));
+
+    return options;
   }
 }
 
