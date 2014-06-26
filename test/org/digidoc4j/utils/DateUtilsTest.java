919987372b26e2cd8cbbe8955c9302cd311f8fd8
Codeborne <xp@codeborne.com>
2014-06-26 10:14:44 +0300

    Added tests

--- test/org/digidoc4j/utils/DateUtilsTest.java
+++ test/org/digidoc4j/utils/DateUtilsTest.java
@@ -1,8 +1,8 @@
 package org.digidoc4j.utils;
 
-import java.util.Date;
-
 import org.junit.Test;
+
+import java.util.Date;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -15,26 +15,26 @@ public class DateUtilsTest {
   }
 
   @Test
-  public void testIsAlmostNowComparedTo10SecondsBeforeNow() throws Exception {
-    Date nowMinus10Seconds = org.apache.commons.lang.time.DateUtils.addSeconds(new Date(), -1);
+  public void testIsAlmostNowComparedTo4SecondsBeforeNow() throws Exception {
+    Date nowMinus10Seconds = org.apache.commons.lang.time.DateUtils.addSeconds(new Date(), -4);
     assertTrue(DateUtils.isAlmostNow(nowMinus10Seconds));
   }
 
   @Test
-  public void testIsAlmostNowComparedTo10SecondsAfterNow() throws Exception {
-    Date nowMinus10Seconds = org.apache.commons.lang.time.DateUtils.addSeconds(new Date(), 1);
-    assertTrue(DateUtils.isAlmostNow(nowMinus10Seconds));
+  public void testIsAlmostNowComparedTo4SecondsAfterNow() throws Exception {
+    Date nowPlus10Seconds = org.apache.commons.lang.time.DateUtils.addSeconds(new Date(), 4);
+    assertTrue(DateUtils.isAlmostNow(nowPlus10Seconds));
   }
 
   @Test
   public void testIsAlmostNowComparedTo40SecondsBeforeNow() throws Exception {
-    Date nowMinus10Seconds = org.apache.commons.lang.time.DateUtils.addSeconds(new Date(), -6);
-    assertFalse(DateUtils.isAlmostNow(nowMinus10Seconds));
+    Date nowMinus40Seconds = org.apache.commons.lang.time.DateUtils.addSeconds(new Date(), -40);
+    assertFalse(DateUtils.isAlmostNow(nowMinus40Seconds));
   }
 
   @Test
   public void testIsAlmostNowComparedTo40SecondsAfterNow() throws Exception {
-    Date nowMinus10Seconds = org.apache.commons.lang.time.DateUtils.addSeconds(new Date(), 6);
-    assertFalse(DateUtils.isAlmostNow(nowMinus10Seconds));
+    Date nowPlus40Seconds = org.apache.commons.lang.time.DateUtils.addSeconds(new Date(), 40);
+    assertFalse(DateUtils.isAlmostNow(nowPlus40Seconds));
   }
 }
--- test/org/digidoc4j/utils/HelperTest.java
+++ test/org/digidoc4j/utils/HelperTest.java
@@ -1,15 +1,38 @@
 package org.digidoc4j.utils;
 
-import java.io.File;
-
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class HelperTest {
 
   @Test
   public void testIsXMLFileWhenFileIsNotXMLFile() throws Exception {
     assertFalse(Helper.isXMLFile(new File("test.txt")));
+  }
+
+  @Test
+  public void testIsXMLFileWhenFileIsXMLFile() throws Exception {
+    assertTrue(Helper.isXMLFile(new File("test.xml")));
+  }
+
+  @Test
+  public void testIsZIPFileWhenFileIsNotZIPFile() throws Exception {
+    assertFalse(Helper.isZipFile(new File("test.txt")));
+  }
+
+  @Test
+  public void testIsZIPFileWhenFileIsZIPFile() throws Exception {
+    FileOutputStream fileOutputStream = new FileOutputStream("test.zip");
+    ZipOutputStream zipOutputStream = new ZipOutputStream(fileOutputStream);
+    zipOutputStream.putNextEntry(new ZipEntry("test.txt"));
+    zipOutputStream.closeEntry();
+    assertTrue(Helper.isZipFile(new File("test.zip")));
   }
 }
