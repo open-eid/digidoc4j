47f796c673ef046351103252092951517de5fce6
Aho Augasmägi <aho@codeborne.com>
2014-07-17 14:24:59 +0300

    removes obsolete code

--- src/org/digidoc4j/utils/PKCS11Signer.java
+++ src/org/digidoc4j/utils/PKCS11Signer.java
@@ -4,15 +4,15 @@ import eu.europa.ec.markt.dss.signature.token.Pkcs11SignatureToken;
 import org.digidoc4j.api.Configuration;
 import org.digidoc4j.api.Signer;
 
+/**
+ * This signer implementation is for testing purposes
+ */
+@Deprecated
 public class PKCS11Signer extends Signer {
 
-  private final Configuration configuration;
-
   public PKCS11Signer(char[] password) {
-    System.setProperty("java.security.debug", "sunpkcs11,pkcs11");
-
-    configuration = new Configuration();
-    signatureTokenConnection = new Pkcs11SignatureToken(configuration.getPKCS11ModulePath(), password, 1);
+    Configuration configuration = new Configuration();
+    signatureTokenConnection = new Pkcs11SignatureToken(configuration.getPKCS11ModulePath(), password, 2);
     keyEntry = signatureTokenConnection.getKeys().get(0);
   }
 
