f4e53161fffa7b7b5381abea43548c5136520cb0
Aho Augasmägi <aho@codeborne.com>
2014-07-17 14:34:36 +0300

    adds pkcs11Signer test

---
+++ test/org/digidoc4j/utils/PKCS11SignerTest.java
@@ -0,0 +1,22 @@
+package org.digidoc4j.utils;
+
+import org.digidoc4j.api.Container;
+import org.digidoc4j.api.Signer;
+
+import java.security.NoSuchAlgorithmException;
+
+public class PKCS11SignerTest {
+
+  public static void signWithIDCard() throws NoSuchAlgorithmException {
+    Container container = Container.create(Container.DocumentType.ASIC_S);
+    Signer pkcs11Signer = new PKCS11Signer("01497".toCharArray());
+    container.addDataFile("testFiles/test.txt", "text/plain");
+    container.sign(pkcs11Signer);
+    container.save("signWithIDCard.ddoc");
+  }
+
+  // must be run with parameter -Djava.security.debug=sunpkcs11,pkcs11
+  public static void main(String[] args) throws NoSuchAlgorithmException {
+    signWithIDCard();
+  }
+}
