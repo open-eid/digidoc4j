7d11817a836b70caa737faa76b7fe6b6ec14b51f
Codeborne <xp@codeborne.com>
2014-07-28 17:32:38 +0300

    Added functionality for creating DigiDoc4JException

--- src/org/digidoc4j/api/exceptions/DigiDoc4JException.java
+++ src/org/digidoc4j/api/exceptions/DigiDoc4JException.java
@@ -5,6 +5,23 @@ package org.digidoc4j.api.exceptions;
  */
 public class DigiDoc4JException extends RuntimeException {
 
+  int errorCode = 0;
+
+  /**
+   * Constructs a new runtime exception with the specified detail message and
+   * cause.  <p>Note that the detail message associated with
+   * {@code cause} is <i>not</i> automatically incorporated in
+   * this runtime exception's detail message.
+   *
+   * @param error   - error code
+   * @param message the detail message (which is saved for later retrieval
+   *                by the {@link #getMessage()} method).
+   */
+  public DigiDoc4JException(int error, String message) {
+    super(message);
+    errorCode = error;
+  }
+
   /**
    * Constructs a new runtime exception with the specified detail message and
    * cause.  <p>Note that the detail message associated with
@@ -18,7 +35,20 @@ public class DigiDoc4JException extends RuntimeException {
     super(message);
   }
 
+  /**
+   * Creates new exception based on another exception
+   *
+   * @param e parent exception
+   */
   public DigiDoc4JException(Exception e) {
     super(e.getMessage(), e.getCause());
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder msg = new StringBuilder();
+    if (errorCode != 0) msg.append("ERROR: ").append(errorCode).append(" - ");
+    msg.append(getMessage());
+    return msg.toString();
   }
 }
---
+++ test/org/digidoc4j/api/exceptions/DigiDoc4JExceptionTest.java
@@ -0,0 +1,22 @@
+package org.digidoc4j.api.exceptions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DigiDoc4JExceptionTest {
+
+  @Test
+  public void toStringWithNoErrorCode() throws Exception {
+    DigiDoc4JException error = new DigiDoc4JException("Error");
+    assertEquals("Error", error.toString());
+  }
+
+  @Test
+  public void toStringWithErrorCode() throws Exception {
+    DigiDoc4JException error = new DigiDoc4JException(5, "Error");
+    assertEquals("ERROR: 5 - Error", error.toString());
+  }
+
+
+}
