b5ecce679dcff80e63910e82636cf055be76c1be
Codeborne <xp@codeborne.com>
2014-09-04 14:49:11 +0300

    Removed unused method

--- src/org/digidoc4j/api/exceptions/NotYetImplementedException.java
+++ src/org/digidoc4j/api/exceptions/NotYetImplementedException.java
@@ -11,13 +11,4 @@ public class NotYetImplementedException extends DigiDoc4JException {
   public NotYetImplementedException() {
     super("Not implemented yet");
   }
-
-  /**
-   * Not implemented yet exception with specific message
-   *
-   * @param message message
-   */
-  public NotYetImplementedException(String message) {
-    super(message);
-  }
 }
