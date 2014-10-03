00103ae134279618ca79e72fb3f448bc527ccfdf
Codeborne <xp@codeborne.com>
2014-10-03 11:27:03 +0300

    removed unused constructor

--- src/org/digidoc4j/main/DigiDoc4JUtilityException.java
+++ src/org/digidoc4j/main/DigiDoc4JUtilityException.java
@@ -2,19 +2,27 @@ package org.digidoc4j.main;
 
 import org.digidoc4j.exceptions.DigiDoc4JException;
 
+/**
+ * Exceptions thrown by DigiDoc4JUtility
+ */
 public class DigiDoc4JUtilityException extends DigiDoc4JException {
   private final int errorCode;
 
-  public DigiDoc4JUtilityException(int errorCode, DigiDoc4JException exception) {
-    super("DigiDoc4J utility - " + exception.getClass().getName() + " : " + exception.getMessage() + "\n");
-    this.errorCode = errorCode;
-  }
-
+  /**
+   * Throws exception with error code and message
+   *
+   * @param errorCode error code
+   * @param message   error message
+   */
   public DigiDoc4JUtilityException(int errorCode, String message) {
     super("DigiDoc4J utility - " + message + "\n");
     this.errorCode = errorCode;
   }
 
+  /**
+   * Returns error code
+   * @return error code
+   */
   public int getErrorCode() {
     return errorCode;
   }
