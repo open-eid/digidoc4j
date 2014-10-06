61f967bc846be990710b40510500b09178a8216c
Codeborne <xp@codeborne.com>
2014-10-06 10:34:12 +0300

    #74204956 added javadoc

--- src/org/digidoc4j/exceptions/UnsupportedFormatException.java
+++ src/org/digidoc4j/exceptions/UnsupportedFormatException.java
@@ -1,7 +1,16 @@
 package org.digidoc4j.exceptions;
 
+/**
+ * Exception what describes unsupported container formats
+ */
 public class UnsupportedFormatException extends DigiDoc4JException {
+
+  /**
+   * Constructs new exception
+   *
+   * @param type message to to be shown
+   */
   public UnsupportedFormatException(String type) {
-    super(type);
+    super("Unsupported format: " + type);
   }
 }
