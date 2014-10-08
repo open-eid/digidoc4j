db94369d4e26c635811b56064c5e7c457d3929bd
Codeborne <xp@codeborne.com>
2014-10-08 17:39:18 +0300

    #80295154 updated javadoc for not yet implemented methods

--- src/org/digidoc4j/Container.java
+++ src/org/digidoc4j/Container.java
@@ -198,6 +198,7 @@ public abstract class Container {
 
   /**
    * Adds signature from the input stream to the container.
+   * For BDOC it throws a NotYetImplementedException().
    *
    * @param signatureStream signature to be added to the container
    */
@@ -264,7 +265,8 @@ public abstract class Container {
   public abstract Signature sign(Signer signer, String signatureId);
 
   /**
-   * Sets configuration for container
+   * Sets configuration for container.
+   * For a DDOC Container it throws a NotYetImplementedException.
    *
    * @param conf configuration
    */
