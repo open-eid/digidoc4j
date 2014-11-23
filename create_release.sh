f89d7bbab5b49a714f29df0f8c395dfc0ab19f68
Aho Augasmägi <aho@codeborne.com>
2014-11-23 20:51:07 +0200

    adds skript to create release in github

--- create_release.sh
+++ create_release.sh
@@ -7,20 +7,18 @@ fi
 TOKEN=$(cat token)
 export BUILD_NUMBER=0
 
-#if [[ -d  ${RELEASE_BUILD_DIR} ]]; then
-#    rm -rf ${RELEASE_BUILD_DIR}
-#    echo "$RELEASE_BUILD_DIR is deleted"
-#fi
-#mkdir ${RELEASE_BUILD_DIR}
-#echo "${RELEASE_BUILD_DIR} is created"
+if [[ -d  ${RELEASE_BUILD_DIR} ]]; then
+    rm -rf ${RELEASE_BUILD_DIR}
+fi
+mkdir ${RELEASE_BUILD_DIR}
 pushd ${RELEASE_BUILD_DIR}
 
-#git clone --recursive https://github.com/open-eid/digidoc4j
+git clone --recursive https://github.com/open-eid/digidoc4j
 cd digidoc4j
 
 VERSION=$(git tag -l public* --sort=version:refname | tail -1 | awk -F '-' '{print $2}')
 
-#git checkout tags/public-${VERSION}
+git checkout tags/public-${VERSION}
 
 IFS='. ' read -a version <<< ${VERSION}
 if [[ $1 == "PRERELEASE" ]]; then
@@ -31,8 +29,8 @@ else
     PRERELEASE=false
 fi
 
-#ant -q -f jenkins_build.xml sd-dss
-#ant -q all -Dlib.version=v${FINAL_VERSION}
+ant -q -f jenkins_build.xml sd-dss
+ant -q all -Dlib.version=v${FINAL_VERSION}
 
 CREATE_RELEASE=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "Release v%s","body": "#Release of version %s","draft": false,"prerelease": %s}' ${FINAL_VERSION} ${FINAL_VERSION} ${FINAL_VERSION} ${PRERELEASE})
 RESPONSE=$(curl --data "${CREATE_RELEASE}" https://api.github.com/repos/open-eid/digidoc4j/releases?access_token=${TOKEN})
