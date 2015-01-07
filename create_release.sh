21f6db23a024a7eed20cd4fb771c1d34aa08a58c
Codeborne <xp@codeborne.com>
2015-01-07 11:04:01 +0200

    Updated naming

--- create_release.sh
+++ create_release.sh
@@ -22,7 +22,7 @@ git checkout tags/public-${VERSION}
 
 IFS='. ' read -a version <<< ${VERSION}
 if [[ $1 == "PRERELEASE" ]]; then
-    FINAL_VERSION=${version[0]}"."${version[1]}"-beta."${version[2]}
+    FINAL_VERSION=${version[0]}"."${version[1]}"."${version[2]}
     PRERELEASE=true
 else
     FINAL_VERSION=${version[0]}.${version[1]}.${version[2]}
@@ -34,7 +34,7 @@ ant -q -f jenkins_build.xml sd-dss
 ant -q all -Dlib.version=v${FINAL_VERSION}
 echo "Build done!"
 
-CREATE_RELEASE=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "Release v%s","body": "#Release of version %s","draft": false,"prerelease": %s}' ${FINAL_VERSION} ${FINAL_VERSION} ${FINAL_VERSION} ${PRERELEASE})
+CREATE_RELEASE=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "Release v%s","body": "### Release of version %s","draft": false,"prerelease": %s}' ${FINAL_VERSION} ${FINAL_VERSION} ${FINAL_VERSION} ${PRERELEASE})
 RESPONSE=$(curl --data "${CREATE_RELEASE}" https://api.github.com/repos/open-eid/digidoc4j/releases?access_token=${TOKEN})
 RELEASE_ID=$(echo ${RESPONSE} | sed -e 's/[{}]/''/g' | awk -F ', ' '{print $5}' | awk -F': ' '{print $2}')
 
@@ -46,10 +46,11 @@ fi
 echo "Uploading assets for release ${RELEASE_ID}... "
 UPLOAD_URL="https://uploads.github.com/repos/open-eid/digidoc4j/releases/${RELEASE_ID}/assets"
 
-echo curl --fail -s -S -H "Authorization: token ${TOKEN}" -H "Content-Type: application/zip" -X POST ${UPLOAD_URL}?name=digidoc4j-v${VERSION}.0-beta-javadoc.jar --data-binary @dist/digidoc4j-v${VERSION}.0-beta-javadoc.jar
-curl --fail -s -S -H "Authorization: token ${TOKEN}" -H "Content-Type: application/zip" -X POST ${UPLOAD_URL}?name=digidoc4j-v${VERSION}.0-beta-javadoc.jar --data-binary @dist/digidoc4j-v${VERSION}.0-beta-javadoc.jar
-curl --fail -s -S -H "Authorization: token ${TOKEN}" -H "Content-Type: application/zip" -X POST ${UPLOAD_URL}?name=digidoc4j-v${VERSION}.0-beta.jar --data-binary @dist/digidoc4j-v${VERSION}.0-beta.jar
-curl --fail -s -S -H "Authorization: token ${TOKEN}" -H "Content-Type: application/zip" -X POST ${UPLOAD_URL}?name=digidoc4j-library-v${VERSION}.0-beta.zip --data-binary @dist/digidoc4j-library-v${VERSION}.0-beta.zip
+echo name=digidoc4j-v${FINAL_VERSION}.0-javadoc.jar --data-binary @dist/digidoc4j-v${FINAL_VERSION}.0-javadoc.jar
+curl --fail -s -S -H "Authorization: token ${TOKEN}" -H "Content-Type: application/zip" -X POST ${UPLOAD_URL}?name=digidoc4j-v${FINAL_VERSION}.0-beta-javadoc.jar --data-binary @dist/digidoc4j-v${FINAL_VERSION}.0-javadoc.jar
+curl --fail -s -S -H "Authorization: token ${TOKEN}" -H "Content-Type: application/zip" -X POST ${UPLOAD_URL}?name=digidoc4j-v${FINAL_VERSION}.0-beta.jar --data-binary @dist/digidoc4j-v${FINAL_VERSION}.0.jar
+curl --fail -s -S -H "Authorization: token ${TOKEN}" -H "Content-Type: application/zip" -X POST ${UPLOAD_URL}?name=digidoc4j-library-v${FINAL_VERSION}.0-beta.zip --data-binary @dist/digidoc4j-library-v${FINAL_VERSION}.0.zip
+curl --fail -s -S -H "Authorization: token ${TOKEN}" -H "Content-Type: application/zip" -X POST ${UPLOAD_URL}?name=digidoc4j-util-v${FINAL_VERSION}.0-beta.zip --data-binary @dist/digidoc4j-util-v${FINAL_VERSION}.0.zip
 
 popd
 
