a812f96e423bbc9d79c27949ff01999a053c60e5
Rainer Villido <Rainer.Villido@nortal.com>
2016-06-15 12:50:07 +0300

    #74204066 added support for PKCS#11

--- src/org/digidoc4j/signers/PKCS11Signer.java
+++ src/org/digidoc4j/signers/PKCS11Signer.java
@@ -10,43 +10,128 @@
 
 package org.digidoc4j.signers;
 
-import org.digidoc4j.Configuration;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.SignatureException;
+import java.security.cert.X509Certificate;
+import java.util.List;
+
+import org.apache.commons.lang.ArrayUtils;
 import org.digidoc4j.DigestAlgorithm;
 import org.digidoc4j.SignatureToken;
+import org.digidoc4j.exceptions.TechnicalException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.security.cert.X509Certificate;
-
+import eu.europa.esig.dss.DSSUtils;
+import eu.europa.esig.dss.EncryptionAlgorithm;
 import eu.europa.esig.dss.token.AbstractSignatureTokenConnection;
 import eu.europa.esig.dss.token.DSSPrivateKeyEntry;
+import eu.europa.esig.dss.token.KSPrivateKeyEntry;
 import eu.europa.esig.dss.token.Pkcs11SignatureToken;
 
 /**
- * This signer implementation is for testing purposes
+ * Implements PKCS#11 interface for Smart Cards and hardware tokens.
+ *
+ * <p/>
+ *
+ * It can be used for making digital signatures with Smart Cards (ID-Cards), USB tokens (Aladdin USB eToken),
+ * HSM (Hardware Security Module) or other hardware tokens that use PKCS#11 API.
+ *
+ * <p/>
+ *
+ * PKCS#11 module path depends on your operating system and installed smart card or hardware token library.
+ *
+ * <p/>
+ *
+ * If you are using OpenSC (https://github.com/OpenSC/OpenSC/wiki), then <br/>
+ * For Windows, it could be C:\Windows\SysWOW64\opensc-pkcs11.dll, <br/>
+ * For Linux, it could be /usr/lib/x86_64-linux-gnu/opensc-pkcs11.so, <br/>
+ * For OSX, it could be /usr/local/lib/opensc-pkcs11.so <br/>
+ *
  */
-@Deprecated
 public class PKCS11Signer implements SignatureToken {
-  protected AbstractSignatureTokenConnection signatureTokenConnection = null;
-  protected DSSPrivateKeyEntry keyEntry = null;
 
+  private static final Logger logger = LoggerFactory.getLogger(PKCS11Signer.class);
+  private AbstractSignatureTokenConnection signatureTokenConnection;
+  private DSSPrivateKeyEntry privateKeyEntry;
 
   /**
-   * Constructor
+   * Initializes the PKCS#11 token.
    *
-   * @param password password
+   * @param pkcs11ModulePath PKCS#11 module path, depends on your operating system and installed smart card or hardware token library.
+   * @param password  Secret pin code for digital signature.
+   * @param slotIndex Token slot index, depends on the hardware token.
    */
-  public PKCS11Signer(char[] password) {
-    Configuration configuration = Configuration.getInstance();
-    signatureTokenConnection = new Pkcs11SignatureToken(configuration.getPKCS11ModulePath(), password, 2);
-    keyEntry = signatureTokenConnection.getKeys().get(0);
+  public PKCS11Signer(String pkcs11ModulePath, char[] password, int slotIndex) {
+    logger.debug("Initializing PKCS#11 signature token from " + pkcs11ModulePath + " and slot " + slotIndex);
+    signatureTokenConnection = new Pkcs11SignatureToken(pkcs11ModulePath, password, slotIndex);
+  }
+
+  /**
+   * Fetches the private key entries from the hardware token for information purposes.
+   * The actual private key remains on the token and won't be accessible.
+   *
+   * @return list of private key entries.
+   */
+  public List<DSSPrivateKeyEntry> getPrivateKeyEntries() {
+    return signatureTokenConnection.getKeys();
+  }
+
+  /**
+   * For selecting a particular private key to be used for signing.
+   * @param privateKeyEntry
+   */
+  public void usePrivateKeyEntry(DSSPrivateKeyEntry privateKeyEntry) {
+    this.privateKeyEntry = privateKeyEntry;
   }
 
   @Override
   public X509Certificate getCertificate() {
-    return keyEntry.getCertificate().getCertificate();
+    logger.debug("Fetching certificate");
+    return getPrivateKeyEntry().getCertificate().getCertificate();
   }
 
   @Override
   public byte[] sign(DigestAlgorithm digestAlgorithm, byte[] dataToSign) {
-    return new byte[0];
+    try {
+      logger.debug("Signing with " + digestAlgorithm.name());
+      byte[] digestToSign = DSSUtils.digest(digestAlgorithm.getDssDigestAlgorithm(), dataToSign);
+      byte[] digestWithPadding = addPadding(digestToSign, digestAlgorithm);
+      return signDigest(digestWithPadding);
+    } catch(Exception e) {
+      logger.error("Failed to sign with PKCS#11: " + e.getMessage());
+      throw new TechnicalException("Failed to sign with PKCS#11: " + e.getMessage(), e);
+    }
+  }
+
+  private byte[] signDigest(byte[] digestToSign) throws InvalidKeyException, SignatureException, NoSuchAlgorithmException {
+    logger.debug("Signing digest");
+    DSSPrivateKeyEntry privateKeyEntry = getPrivateKeyEntry();
+    PrivateKey privateKey = ((KSPrivateKeyEntry) privateKeyEntry).getPrivateKey();
+    EncryptionAlgorithm encryptionAlgorithm = privateKeyEntry.getEncryptionAlgorithm();
+    String signatureAlgorithm = "NONEwith" + encryptionAlgorithm.getName();
+    return invokeSigning(digestToSign, privateKey, signatureAlgorithm);
+  }
+
+  private byte[] invokeSigning(byte[] digestToSign, PrivateKey privateKey, String signatureAlgorithm) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+    logger.debug("Signing with signature algorithm " + signatureAlgorithm);
+    java.security.Signature signer = java.security.Signature.getInstance(signatureAlgorithm);
+    signer.initSign(privateKey);
+    signer.update(digestToSign);
+    byte[] signatureValue = signer.sign();
+    return signatureValue;
+  }
+
+  private static byte[] addPadding(byte[] digest, DigestAlgorithm digestAlgorithm) {
+    return ArrayUtils.addAll(digestAlgorithm.digestInfoPrefix(), digest); // should find the prefix by checking digest length?
+  }
+
+  private DSSPrivateKeyEntry getPrivateKeyEntry() {
+    if(privateKeyEntry == null) {
+      privateKeyEntry = getPrivateKeyEntries().get(0);
+    }
+    return privateKeyEntry;
   }
 }
--- src/prototype/PKCS11SignerTestMain.java
+++ src/prototype/PKCS11SignerTestMain.java
@@ -10,6 +10,8 @@
 
 package prototype;
 
+import java.security.NoSuchAlgorithmException;
+
 import org.digidoc4j.Container;
 import org.digidoc4j.ContainerBuilder;
 import org.digidoc4j.Signature;
@@ -17,16 +19,35 @@ import org.digidoc4j.SignatureBuilder;
 import org.digidoc4j.SignatureToken;
 import org.digidoc4j.signers.PKCS11Signer;
 
-import java.security.NoSuchAlgorithmException;
-
 public class PKCS11SignerTestMain {
+
+  /**
+   * PKCS#11 module path depends on your operating system and installed smart card or hardware token library.
+   *
+   * If you are using OpenSC (https://github.com/OpenSC/OpenSC/wiki), then
+   * For Windows, it could be C:\Windows\SysWOW64\opensc-pkcs11.dll
+   * For Linux, it could be /usr/lib/x86_64-linux-gnu/opensc-pkcs11.so
+   * For OSX, it could be /usr/local/lib/opensc-pkcs11.so
+   *
+   */
+  public static final String PKCS_11_MODULE_PATH = "/usr/local/lib/opensc-pkcs11.so";
+
+  /**
+   * Secret pin code for digital signature
+   */
+  public static final String PIN_CODE = "22975";
+
+  /**
+   * Token slot index. Depends on the hardware token. Estonian ID-Card slot index is 2.
+   */
+  public static final int SLOT_INDEX = 2;
 
   public static void signWithIDCard() throws NoSuchAlgorithmException {
     Container container = ContainerBuilder.
         aContainer(ContainerBuilder.BDOC_CONTAINER_TYPE).
         withDataFile("testFiles/test.txt", "text/plain").
         build();
-    SignatureToken pkcs11Signer = new PKCS11Signer("01497".toCharArray());
+    SignatureToken pkcs11Signer = new PKCS11Signer(PKCS_11_MODULE_PATH, PIN_CODE.toCharArray(), SLOT_INDEX);
     Signature signature = SignatureBuilder.
         aSignature(container).
         withSignatureToken(pkcs11Signer).
@@ -35,7 +56,6 @@ public class PKCS11SignerTestMain {
     container.saveAsFile("signWithIDCard.ddoc");
   }
 
-  // must be run with parameter -Djava.security.debug=sunpkcs11,pkcs11
   public static void main(String[] args) throws NoSuchAlgorithmException {
     signWithIDCard();
   }
