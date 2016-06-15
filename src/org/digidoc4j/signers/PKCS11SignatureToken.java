8e5d00cadd21e0a79af29a89a7d5c3ae5ed5caf2
Rainer Villido <Rainer.Villido@nortal.com>
2016-06-15 16:37:08 +0300

    #74204066 added support for pkcs11 password callbacks

--- src/org/digidoc4j/signers/PKCS11SignatureToken.java
+++ src/org/digidoc4j/signers/PKCS11SignatureToken.java
@@ -29,27 +29,21 @@ import eu.europa.esig.dss.EncryptionAlgorithm;
 import eu.europa.esig.dss.token.AbstractSignatureTokenConnection;
 import eu.europa.esig.dss.token.DSSPrivateKeyEntry;
 import eu.europa.esig.dss.token.KSPrivateKeyEntry;
+import eu.europa.esig.dss.token.PasswordInputCallback;
 import eu.europa.esig.dss.token.Pkcs11SignatureToken;
 
 /**
  * Implements PKCS#11 interface for Smart Cards and hardware tokens.
- *
  * <p/>
- *
  * It can be used for making digital signatures with Smart Cards (ID-Cards), USB tokens (Aladdin USB eToken),
  * HSM (Hardware Security Module) or other hardware tokens that use PKCS#11 API.
- *
  * <p/>
- *
  * PKCS#11 module path depends on your operating system and installed smart card or hardware token library.
- *
  * <p/>
- *
  * If you are using OpenSC (https://github.com/OpenSC/OpenSC/wiki), then <br/>
  * For Windows, it could be C:\Windows\SysWOW64\opensc-pkcs11.dll, <br/>
  * For Linux, it could be /usr/lib/x86_64-linux-gnu/opensc-pkcs11.so, <br/>
  * For OSX, it could be /usr/local/lib/opensc-pkcs11.so <br/>
- *
  */
 public class PKCS11SignatureToken implements SignatureToken {
 
@@ -61,12 +55,26 @@ public class PKCS11SignatureToken implements SignatureToken {
    * Initializes the PKCS#11 token.
    *
    * @param pkcs11ModulePath PKCS#11 module path, depends on your operating system and installed smart card or hardware token library.
-   * @param password  Secret pin code for digital signature.
-   * @param slotIndex Token slot index, depends on the hardware token.
+   * @param password         Secret pin code for digital signature.
+   * @param slotIndex        Token slot index, depends on the hardware token.
    */
   public PKCS11SignatureToken(String pkcs11ModulePath, char[] password, int slotIndex) {
     logger.debug("Initializing PKCS#11 signature token from " + pkcs11ModulePath + " and slot " + slotIndex);
     signatureTokenConnection = new Pkcs11SignatureToken(pkcs11ModulePath, password, slotIndex);
+  }
+
+  /**
+   * Initializes the PKCS#11 token with password callback.
+   * <p/>
+   * This Password Callback is used in order to retrieve the password from the user when accessing the Key Store.
+   *
+   * @param pkcs11ModulePath PKCS#11 module path, depends on your operating system and installed smart card or hardware token library.
+   * @param passwordCallback callback for providing the password for the private key.
+   * @param slotIndex        Token slot index, depends on the hardware token.
+   */
+  public PKCS11SignatureToken(String pkcs11ModulePath, PasswordInputCallback passwordCallback, int slotIndex) {
+    logger.debug("Initializing PKCS#11 signature token with password callback from " + pkcs11ModulePath + " and slot " + slotIndex);
+    signatureTokenConnection = new Pkcs11SignatureToken(pkcs11ModulePath, passwordCallback, slotIndex);
   }
 
   /**
@@ -81,6 +89,7 @@ public class PKCS11SignatureToken implements SignatureToken {
 
   /**
    * For selecting a particular private key to be used for signing.
+   *
    * @param privateKeyEntry
    */
   public void usePrivateKeyEntry(DSSPrivateKeyEntry privateKeyEntry) {
@@ -100,7 +109,7 @@ public class PKCS11SignatureToken implements SignatureToken {
       byte[] digestToSign = DSSUtils.digest(digestAlgorithm.getDssDigestAlgorithm(), dataToSign);
       byte[] digestWithPadding = addPadding(digestToSign, digestAlgorithm);
       return signDigest(digestWithPadding);
-    } catch(Exception e) {
+    } catch (Exception e) {
       logger.error("Failed to sign with PKCS#11: " + e.getMessage());
       throw new TechnicalException("Failed to sign with PKCS#11: " + e.getMessage(), e);
     }
@@ -129,7 +138,7 @@ public class PKCS11SignatureToken implements SignatureToken {
   }
 
   private DSSPrivateKeyEntry getPrivateKeyEntry() {
-    if(privateKeyEntry == null) {
+    if (privateKeyEntry == null) {
       privateKeyEntry = getPrivateKeyEntries().get(0);
     }
     return privateKeyEntry;
