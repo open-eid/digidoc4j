b156440edd7c52196757a2c3318e6459e07ce462
kamlatm <margus.kamlat@cgi.com>
2017-05-02 15:16:31 +0300

    Support for writing Container directly to OutputStream

--- src/org/digidoc4j/utils/Helper.java
+++ src/org/digidoc4j/utils/Helper.java
@@ -15,14 +15,17 @@ import static eu.europa.esig.dss.SignatureLevel.ASiC_E_BASELINE_LT;
 import static eu.europa.esig.dss.SignatureLevel.ASiC_E_BASELINE_LTA;
 import static java.nio.file.Files.deleteIfExists;
 
+import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.nio.file.Paths;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
