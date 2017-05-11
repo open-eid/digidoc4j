fc940f4cdd1b86d9cb5c66ce02dd97b5dd251ccf
Indrek Jentson <indrek.jentson@cgi.com>
2017-05-11 10:24:33 +0300

    Changes derived from DSS 5.0.
    Signed-off-by: Indrek Jentson <indrek.jentson@cgi.com>

--- test/org/digidoc4j/impl/bdoc/BDocContainerTest.java
+++ test/org/digidoc4j/impl/bdoc/BDocContainerTest.java
@@ -86,6 +86,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import eu.europa.esig.dss.DSSDocument;
+import eu.europa.esig.dss.DomUtils;
 import eu.europa.esig.dss.xades.DSSXMLUtils;
 import eu.europa.esig.dss.xades.XPathQueryHolder;
 import eu.europa.esig.dss.xades.validation.XAdESSignature;
@@ -1164,7 +1165,8 @@ public class BDocContainerTest extends DigiDoc4JTestHelper {
   private int countOcspResponderCertificates(XAdESSignature xAdESSignature) {
     XPathQueryHolder xPathQueryHolder = xAdESSignature.getXPathQueryHolder();
     String xPath = xPathQueryHolder.XPATH_CERTIFICATE_VALUES;
-    Element certificateValues = DSSXMLUtils.getElement(xAdESSignature.getSignatureElement(), xPath);
+    // TODO: check and test
+    Element certificateValues = DomUtils.getElement(xAdESSignature.getSignatureElement(), xPath);
     return countResponderCertIdInsCertificateValues(certificateValues);
   }
 
