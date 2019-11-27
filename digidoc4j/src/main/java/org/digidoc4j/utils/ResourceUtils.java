5a11482e5eb2682f32a5e243fac2e223218c3a52
Risto Seene <39149669+rsarendus@users.noreply.github.com>
2019-11-27 13:42:15 +0200

    DD4J-332 Added debug logging for ResourceUtils on failures

--- digidoc4j/src/main/java/org/digidoc4j/utils/ResourceUtils.java
+++ digidoc4j/src/main/java/org/digidoc4j/utils/ResourceUtils.java
@@ -1,15 +1,21 @@
 package org.digidoc4j.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public final class ResourceUtils {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ResourceUtils.class);
+
     public static boolean isResourceAccessible(String path) {
         try {
             return ResourceUtils.class.getClassLoader().getResource(path) != null;
         } catch (RuntimeException e) {
+            LOGGER.debug("Failed to acquire resource URL for path: " + path, e);
             return false;
         }
     }
@@ -19,6 +25,7 @@ public final class ResourceUtils {
             Path pathToFile = Paths.get(path);
             return Files.isRegularFile(pathToFile) && Files.isReadable(pathToFile);
         } catch (RuntimeException e) {
+            LOGGER.debug("Failed to check if path exists as a regular file and is readable: " + path, e);
             return false;
         }
     }
