e905d3c956f454496d8afa1c77b5eb2db2fba1a4
Risto Seene <39149669+rsarendus@users.noreply.github.com>
2019-11-29 13:08:24 +0200

    DD4J-512 Added check to ensure relevant non-LT_TM signatures also contain OCSP response after signing; added suite of tests to ensure creating incomplete signatures is not possible

--- digidoc4j/src/test/java/org/digidoc4j/CertificateValidatorBuilderTest.java
+++ digidoc4j/src/test/java/org/digidoc4j/CertificateValidatorBuilderTest.java
@@ -83,7 +83,7 @@ public class CertificateValidatorBuilderTest extends AbstractTest {
   public void testLoadingOCSPIntermediateCertificatesFromCustomLocation() {
     ExtendedCertificateSource source = CertificateValidatorBuilder.getDefaultCertificateSource();
     source.importFromPath(Paths.get("src/test/resources/testFiles/certs"));
-    Assert.assertEquals("Not equals", 9, source.getCertificatePool().getNumberOfCertificates());
+    Assert.assertEquals("Not equals", 10, source.getCertificatePool().getNumberOfCertificates());
   }
 
   /*
