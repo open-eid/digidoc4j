9b2fe21854ed0c68c698ed0d0cf58292edc8f533
Risto Seene <39149669+rsarendus@users.noreply.github.com>
2022-05-13 12:11:50 +0300

    DD4J-768 Update dependencies

--- digidoc4j/src/test/java/org/digidoc4j/impl/UncompressedAsicEntryTest.java
+++ digidoc4j/src/test/java/org/digidoc4j/impl/UncompressedAsicEntryTest.java
@@ -1,3 +1,13 @@
+/* DigiDoc4J library
+ *
+ * This software is released under either the GNU Library General Public
+ * License (see LICENSE.LGPL).
+ *
+ * Note that the only valid version of the LGPL license as far as this
+ * project is concerned is the original GNU Library General Public License
+ * Version 2.1, February 1999
+ */
+
 package org.digidoc4j.impl;
 
 import org.digidoc4j.impl.asic.AsicEntry;
@@ -58,7 +68,7 @@ public class UncompressedAsicEntryTest {
     ZipEntry derivedZipEntry = asicEntry.getZipEntry();
 
     assertZipEntry(derivedZipEntry, 3L, 345L);
-    Mockito.verifyZeroInteractions(supplierMock);
+    Mockito.verifyNoInteractions(supplierMock);
   }
 
   @Test
