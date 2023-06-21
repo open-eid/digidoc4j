110cbcc05b7986927091fdb5121dd75333747d11
Risto Seene <risto.seene@nortal.com>
2023-06-21 10:51:16 +0300

    DD4J-869 Use LinkedHashMap in ManifestParser to retain the original order of the parsed elements

--- digidoc4j/src/main/java/org/digidoc4j/impl/asic/manifest/ManifestEntry.java
+++ digidoc4j/src/main/java/org/digidoc4j/impl/asic/manifest/ManifestEntry.java
@@ -1,18 +1,19 @@
 /* DigiDoc4J library
-*
-* This software is released under either the GNU Library General Public
-* License (see LICENSE.LGPL).
-*
-* Note that the only valid version of the LGPL license as far as this
-* project is concerned is the original GNU Library General Public License
-* Version 2.1, February 1999
-*/
+ *
+ * This software is released under either the GNU Library General Public
+ * License (see LICENSE.LGPL).
+ *
+ * Note that the only valid version of the LGPL license as far as this
+ * project is concerned is the original GNU Library General Public License
+ * Version 2.1, February 1999
+ */
 
 package org.digidoc4j.impl.asic.manifest;
 
 import java.io.Serializable;
 import java.util.Objects;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,15 +21,17 @@ import org.slf4j.LoggerFactory;
  * Contains information of filenames and mimetypes.
  */
 public final class ManifestEntry implements Serializable{
+
   private static final Logger logger = LoggerFactory.getLogger(ManifestEntry.class);
-  private String fileName;
-  private String mimeType;
+
+  private final String fileName;
+  private final String mimeType;
 
   /**
    * ManifestEntry constructor
    *
-   * @param fileName
-   * @param mimeType
+   * @param fileName filename
+   * @param mimeType mimetype
    */
   public ManifestEntry(String fileName, String mimeType) {
     this.fileName = fileName;
@@ -58,10 +61,9 @@ public final class ManifestEntry implements Serializable{
   @Override
   public boolean equals(Object obj) {
     if (obj instanceof ManifestEntry) {
-      if (fileName.equals(((ManifestEntry)obj).getFileName())
-          && mimeType.equals(((ManifestEntry)obj).getMimeType())) {
-        return true;
-      }
+      ManifestEntry manifestEntry = (ManifestEntry) obj;
+      return StringUtils.equals(fileName, manifestEntry.getFileName())
+              && StringUtils.equals(mimeType, manifestEntry.getMimeType());
     }
     return false;
   }
@@ -70,4 +72,5 @@ public final class ManifestEntry implements Serializable{
   public int hashCode() {
     return Objects.hash(fileName, mimeType);
   }
+
 }
