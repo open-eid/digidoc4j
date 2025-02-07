4c009ad3d0c8ba6b67a669943b050ae992fbf5a1
Risto Seene <risto.seene@nortal.com>
2025-02-07 14:41:19 +0200

    DD4J-1222 Improve thread-safety of legacy JDigiDoc configuration initialization for DDoc container validation

--- digidoc4j/src/main/java/org/digidoc4j/impl/ddoc/ConfigManagerInitializer.java
+++ digidoc4j/src/main/java/org/digidoc4j/impl/ddoc/ConfigManagerInitializer.java
@@ -1,18 +1,20 @@
 /* DigiDoc4J library
-*
-* This software is released under either the GNU Library General Public
-* License (see LICENSE.LGPL).
-*
-* Note that the only valid version of the LGPL license as far as this
-* project is concerned is the original GNU Library General Public License
-* Version 2.1, February 1999
-*/
+ *
+ * This software is released under either the GNU Library General Public
+ * License (see LICENSE.LGPL).
+ *
+ * Note that the only valid version of the LGPL license as far as this
+ * project is concerned is the original GNU Library General Public License
+ * Version 2.1, February 1999
+ */
 
 package org.digidoc4j.impl.ddoc;
 
 import java.io.Serializable;
 
 import org.digidoc4j.Configuration;
+import org.digidoc4j.ddoc.DigiDocException;
+import org.digidoc4j.exceptions.TechnicalException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -44,6 +46,7 @@ public class ConfigManagerInitializer implements Serializable{
     logger.info("Initializing DDoc configuration manager");
     ConfigManager.init(configuration.getDDoc4JConfiguration());
     ConfigManager.addProvider();
+    triggerLazilyLoadableConfigurationInitialization();
     configManagerInitialized = true;
   }
 
@@ -58,6 +61,31 @@ public class ConfigManagerInitializer implements Serializable{
     //Using double-checked locking to avoid other threads to start initialization
     if(!configManagerInitialized) {
       forceInitConfigManager(configuration);
+    }
+  }
+
+  private static void triggerLazilyLoadableConfigurationInitialization() {
+    ConfigManager configManager = ConfigManager.instance();
+    // The following factories inside the JDigiDoc configuration are not loaded during the initialization of the
+    //  ConfigManager. They are loaded and initialized lazily when they are requested from the ConfigManager for the
+    //  first time. This can happen anywhere in DDoc container loading or validation logic, outside the synchronized
+    //  block of the ConfigManagerInitializer.
+    // Request the relevant factories from the ConfigManager instance to ensure that their loading and initialization
+    //  is triggered inside the synchronized block of the ConfigManagerInitializer.
+    try {
+      configManager.getCanonicalizationFactory();
+    } catch (DigiDocException e) {
+      throw new TechnicalException("Failed to acquire DigiDoc canonicalization factory", e);
+    }
+    try {
+      configManager.getNotaryFactory();
+    } catch (DigiDocException e) {
+      throw new TechnicalException("Failed to acquire DigiDoc notary factory", e);
+    }
+    try {
+      configManager.getTslFactory();
+    } catch (DigiDocException e) {
+      throw new TechnicalException("Failed to acquire DigiDoc TSL factory", e);
     }
   }
 
