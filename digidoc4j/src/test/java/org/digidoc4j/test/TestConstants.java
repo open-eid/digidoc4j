6ff299218d27b95c760271963958df09f88f1169
Ron Arnar Pehka <ronarnar.pehka@nortal.com>
2026-04-08 15:52:44 +0300

    DD4J-1405 Update test constant

--- digidoc4j/src/test/java/org/digidoc4j/test/TestConstants.java
+++ digidoc4j/src/test/java/org/digidoc4j/test/TestConstants.java
@@ -60,12 +60,12 @@ public final class TestConstants {
   public static final String DEMO_SK_ESTEID2015_OCSP_2026_CN = "DEMO of ESTEID-SK 2015 OCSP RESPONDER 2026";
   public static final String DEMO_SK_ESTEID2018_OCSP_2018_CN = "DEMO of ESTEID-SK 2018 AIA OCSP RESPONDER 2018";
   public static final String DEMO_SK_ESTEID2018_OCSP_2026_CN = "DEMO of ESTEID2018 OCSP RESPONDER 2026";
-  public static final String TEST_ESTEID2018_OCSP_2026_03_CN = "TEST of ESTEID2018 OCSP RESPONDER 202603";
+  public static final String TEST_ESTEID2018_OCSP_2026_04_CN = "TEST of ESTEID2018 OCSP RESPONDER 202604";
 
   // CN of currently active OCSP responders
   public static final String DEMO_SK_ESTEID2015_OCSP_CN = DEMO_SK_ESTEID2015_OCSP_2026_CN;
   public static final String DEMO_SK_ESTEID2018_OCSP_CN = DEMO_SK_ESTEID2018_OCSP_2026_CN;
-  public static final String TEST_ESTEID2018_OCSP_CN = TEST_ESTEID2018_OCSP_2026_03_CN;
+  public static final String TEST_ESTEID2018_OCSP_CN = TEST_ESTEID2018_OCSP_2026_04_CN;
 
   private TestConstants() {
   }
